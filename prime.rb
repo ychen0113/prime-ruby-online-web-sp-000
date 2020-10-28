5c14195661135fdb55d2f1ff33256c0eecec42fd
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-28 22:38:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(n)
+  i = 5
+  if n <= 1 
+   false
+    elsif n <= 3
+     true 
+    elsif n % 2 == 0 or n % 3 == 0 
+     false
+    elsif n > 3 
+      while i * i <= n do 
+        return false if n % i == 0 or n % (i + 2) == 0
+        i += 6
+        end
+        true
+
+  end 
+end
